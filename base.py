ff497495c5737a4c0d78c4b6171ff28e81337c78
Vitor Figueiredo Marques <vitorfigmarques@hotmail.com>
2020-07-21 06:56:09 -0300

    Delete base.py

--- base.py
+++
@@ -1,5 +0,0 @@
-dir = 'C:/Users/Vitor/Desktop/Programação/Data-Science/técnicas/files/'
-import pandas as pd
-credit_Data = pd.read_csv(dir+'credit_data.csv')
-census = pd.read_csv(dir+'census.csv')
-risco_credit = pd.read_csv(dir+'risco_credito.csv')
